abe72c0b795c80ae76662062d10b69e86555d66d
Tianhan Lu <tianhan.lu@colorado.edu>
2021-01-27 15:35:29 -0500

    more benchmarks

--- src/main/java/brbo/benchmarks/containers/log4j2/GetAllDeclaredFields.java
+++ src/main/java/brbo/benchmarks/containers/log4j2/GetAllDeclaredFields.java
@@ -3,7 +3,18 @@ package brbo.benchmarks.containers.log4j2;
 import brbo.benchmarks.Common;
 
 abstract public class GetAllDeclaredFields extends Common {
-  void f() {
-    ;
+  void f(int cls) {
+    if (cls <= 0)
+      return;
+    int iterator = cls;
+    int R = 0;
+    boundAssertion(R <= cls);
+    int fields = 0;
+    while (iterator > 0) {
+      int entry = ndInt2(1, iterator);
+      iterator -= entry;
+      fields += entry;
+      R = R + entry;
+    }
   }
 }
