dfb81eb68780ca47222d6fe21917b1fa5648bd48
Tianhan Lu <tianhan.lu@colorado.edu>
2021-05-06 15:26:31 -0400

    refactor

--- artifacts/sas21/benchmarks/all.sh
+++ artifacts/sas21/benchmarks/all.sh
@@ -8,8 +8,4 @@ mostPrecise="mostPrecise"
 
 mkdir -p output/
 
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/containers -a all -t $timeout > output/"containers-$today-$timeout-$mostPrecise.txt" 2>&1
-
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/string -a all -t $timeout > output/"string-$today-$timeout-$mostPrecise.txt" 2>&1
-
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/synthetic -a all -t $timeout > output/"synthetic-$today-$timeout-$mostPrecise.txt" 2>&1
+./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/all -a all -t $timeout > output/"containers-$today-$timeout-$mostPrecise.txt" 2>&1
--- artifacts/sas21/benchmarks/less-precise/all.sh
+++ artifacts/sas21/benchmarks/less-precise/all.sh
@@ -8,8 +8,4 @@ lessPrecise="lessPrecise"
 
 mkdir -p output/
 
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/containers -a all -t $timeout --less-precise > output/"containers-$today-$timeout-$lessPrecise.txt" 2>&1
-
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/string -a all -t $timeout --less-precise > output/"string-$today-$timeout-$lessPrecise.txt" 2>&1
-
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/synthetic -a all -t $timeout --less-precise > output/"synthetic-$today-$timeout-$lessPrecise.txt" 2>&1
+./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/ -a all -t $timeout --less-precise > output/"containers-$today-$timeout-$lessPrecise.txt" 2>&1
