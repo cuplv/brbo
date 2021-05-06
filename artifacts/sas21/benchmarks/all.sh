0b40ea2324d9bb4a37b443e71f05d8741a0ca92d
Tianhan Lu <Tianhan.Lu@colorado.edu>
2021-05-06 16:21:52 +0000

    update scripts

--- artifacts/sas21/benchmarks/all.sh
+++ artifacts/sas21/benchmarks/all.sh
@@ -6,6 +6,8 @@ today=$(date +%m%d-%H%M)
 timeout=60
 mostPrecise="mostPrecise"
 
+mkdir -p output/
+
 ./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/containers -a all -t $timeout > output/"containers-$today-$timeout-$mostPrecise.txt" 2>&1
 
 ./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/string -a all -t $timeout > output/"string-$today-$timeout-$mostPrecise.txt" 2>&1
--- artifacts/sas21/benchmarks/less-precise/all.sh
+++ artifacts/sas21/benchmarks/less-precise/all.sh
@@ -6,6 +6,8 @@ today=$(date +%m%d-%H%M)
 timeout=60
 lessPrecise="lessPrecise"
 
+mkdir -p output/
+
 ./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/containers -a all -t $timeout --less-precise > output/"containers-$today-$timeout-$lessPrecise.txt" 2>&1
 
 ./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/string -a all -t $timeout --less-precise > output/"string-$today-$timeout-$lessPrecise.txt" 2>&1
