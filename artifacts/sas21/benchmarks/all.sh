1984de1a3c11feb230595a3272a50eddcee75818
Tianhan Lu <tianhan.lu@colorado.edu>
2021-05-06 15:27:40 -0400

    fix a typo

--- artifacts/sas21/benchmarks/all.sh
+++ artifacts/sas21/benchmarks/all.sh
@@ -8,4 +8,4 @@ mostPrecise="mostPrecise"
 
 mkdir -p output/
 
-./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/all -a all -t $timeout > output/"containers-$today-$timeout-$mostPrecise.txt" 2>&1
+./scripts/run_with_deps_artifact.sh -d src/main/java/brbo/benchmarks/ -a all -t $timeout > output/"containers-$today-$timeout-$mostPrecise.txt" 2>&1
